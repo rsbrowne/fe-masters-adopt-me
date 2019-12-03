d8ecd2aa0313bfb3fc344fbafc6d2b7d06990514
Ryan Browne <brownetown7@gmail.com>
2019-12-03 20:36:02 +0000

    Prettier setup

--- src/App.js
+++ src/App.js
@@ -7,19 +7,20 @@ const Pet = ({ name, animal, breed }) => {
 };
 
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    [
-      React.createElement("h1", {}, "Adopt Me!"),
-      React.createElement(Pet, { name: "Luna", animal: "Dog", breed: "Havanese"}),
-      React.createElement(Pet, { name: "Streak", animal: "Bird", breed: "Cockatiel"}),
-      React.createElement(Pet, { name: "Shadow", animal: "Cat", breed: "Black"})
-    ]
-  );
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese"
+    }),
+    React.createElement(Pet, {
+      name: "Streak",
+      animal: "Bird",
+      breed: "Cockatiel"
+    }),
+    React.createElement(Pet, { name: "Shadow", animal: "Cat", breed: "Black" })
+  ]);
 };
 
-ReactDOM.render(
-  React.createElement(App),
-  document.getElementById("root")
-);
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
