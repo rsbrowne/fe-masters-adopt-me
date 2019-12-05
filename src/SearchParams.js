86788c975c050e041a5ad5180f8ca904ee9ebd88
Ryan Browne <brownetown7@gmail.com>
2019-12-05 22:52:34 +0000

    Adds basic state and eslint state rules

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const SearchParams = () => {
-  const location = "Seattle, WA";
+  const [location, setLocation] = useState('Seattle, WA');
 
   return (
     <div className="search-params">
       <form >
         <label htmlFor="location">
           location
-          <input 
+          <input
             type="text"
             id="location"
             value={location}
             placeholder="location"
+            onChange={e => setLocation(e.target.value)}
           />
         </label>
         <button>Submit</button>
