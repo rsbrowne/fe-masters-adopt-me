ec0babb64f2198ffc66555a0cf6eec8fd74cc477
Ryan Browne <brownetown7@gmail.com>
2020-01-29 21:25:42 +0000

    Adds some minor animations

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Link } from '@reach/router';
-import { css } from '@emotion/core';
+import { css, keyframes } from '@emotion/core';
 import { useState } from 'react';
 import colours from './colours';
 
 const color = colours.secondary;
+const spin = keyframes`
+  to {
+    transform: rotate(360deg);
+  }
+`;
 
 const NavBar = () => {
   const [padding, setPadding] = useState(15);
+  const [spinSpeed, setSpinSpeed] = useState(5);
 
   return (
     <header
@@ -19,11 +25,15 @@ const NavBar = () => {
     >
       <Link to="/">Adopt Me!</Link>
       <span
+        onClick={() => setSpinSpeed(spinSpeed/2)}
         css={css`
           font-size: 60px;
+          display: inline-block;
+          animation: ${spinSpeed}s ${spin} linear infinite;
 
           &:hover {
             text-decoration: underline;
+            animation: ${spinSpeed}s ${spin} linear infinite reverse;
           }
         `}
         role="img"
